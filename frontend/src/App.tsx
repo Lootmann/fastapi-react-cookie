8b3477e2ea2597b788906eefd53086b25164cd6b
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-03-28 23:25:28 +0900

    frontend: first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-zinc-900 text-zinc-200">
+      <div className="p-4 text-xl">hello world</div>
+    </div>
+  );
+}
+
+export default App;
